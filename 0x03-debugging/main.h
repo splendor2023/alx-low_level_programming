5b7c9cdc253d87a83b99ef01c311e7d07f2f6c92
splendor2023 <uchechukwu735@gmail.com>
2023-03-23 13:46:32 -0700

    3. Leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,5 +3,8 @@
 
 #include <stdio.h>
 
+void positive_or_negative(int i);
+int main(void)
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year)
 #endif /* MAIN_H */
