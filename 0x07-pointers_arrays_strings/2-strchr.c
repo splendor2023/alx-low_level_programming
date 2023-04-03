764788b7db2e987b535752f751ab10e6d1b1abf2
splendor2023 <uchechukwu735@gmail.com>
2023-04-03 15:20:18 -0700

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - entry point
+ *@s: input
+ *@c: input
+ *Return: Always 0 (success)
+ */
+char *_strchr(char *s, char c)
+{
+	int j = 0;
+
+	for (; s[j] >= '\0'; j++)
+	{
+	if (s[j] == c)
+	return (&s[i]);
+	}
+	return (0)
+}
