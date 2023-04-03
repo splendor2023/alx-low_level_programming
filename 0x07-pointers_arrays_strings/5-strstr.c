6dab682b89247da6df3353ea90ce499903b2c198
splendor2023 <uchechukwu735@gmail.com>
2023-04-03 15:56:14 -0700

    function that locates a sustring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *q = haystack;
+		char *p = needle;
+
+	while (*q == *p && *p != '\0')
+		{
+			q++;
+			p++;
+		}
+
+		if (*p == '\0')
+	return (haystack);
+	}
+
+	return (0);
+}
