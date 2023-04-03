c2d50dce6fc235cc33b314a5cf0aea2023b74ccd
splendor2023 <uchechukwu735@gmail.com>
2023-04-03 15:41:32 -0700

    corrections done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,19 +8,19 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
-	int q;
+	int r;
 
 	while (*s)
 	{
-	for (r = 0; accept[q]; q++)
-	{
-	if (*s == accept[q])
-	{
-	n++;
-	break;
-	}
-	else if (accept[q + 1] == '\0')
-	return (n);
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
 		}
 		s++;
 	}
