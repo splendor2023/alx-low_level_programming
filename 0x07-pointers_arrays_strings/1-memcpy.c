825df9bc1ea14a1b35b076f726a4ee177203d6cc
splendor2023 <uchechukwu735@gmail.com>
2023-04-03 15:01:08 -0700

    corrections on the file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h
 /**
-* _memcpy - a fubction that copies memory area
-*@src: memory where bytes is copied
-*@dest: memory where bytes is stored
-*@n: number of byte
-*
-* Return: copied memory with n byte changed
-*/
+ * _memcpy - a fubction that copies memory area
+ *@src: memory where bytes is copied
+ *@dest: memory where bytes is stored
+ *@n: number of byte
+ *
+ * Return: copied memory with n byte changed
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int y = 0;
