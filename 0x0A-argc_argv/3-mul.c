2fb1362745e80d3bd258db28a8d7f3d90edf3bad
splendor2023 <uchechukwu735@gmail.com>
2023-04-10 21:56:36 -0700

    edit the file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,6 @@ int _atoi(char *s)
 	digit = 0;
 
 	while (s[len] != '\0')
-
 		len++;
 
 	while (i < len && f == 0)
@@ -27,22 +26,29 @@ int _atoi(char *s)
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-		digit = s[i] - '0';
+			digit = s[i] - '0';
 
-		if (d % 2)
-		digit = -digit;
-		n = n * 10 + digit;
-		f = 1;
-	if (s[i + 1] < '0' || s[i + 1] > '9')
-		break;
-		f = 0;
+			if (d % 2)
+
+				digit = -digit;
+
+			n = n * 10 + digit;
+
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+
+				break;
+			f = 0;
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
+
 /**
 * main - multiplies two numbers
 * @argc: number of arguments
