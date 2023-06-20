afaf4b358629b18bb5da5e6e8c8e0f97ed9c4929
splendor2023 <uchechukwu735@gmail.com>
2023-06-19 20:19:48 -0700

    dynamic lib

---
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *j = haystack;
+		char *p = needle;
+
+	while (*j == *p && *p != '\0')
+	{
+		j++;
+		p++;
+	}
+
+	if (*p == '\0')
+	return (haystack);
+	}
+
+
+	return (0);
+}
